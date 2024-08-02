b8abf88f4d9ac769024008e436cf1c85895548f1
foy4748 <faisaljfcl@gmail.com>
2024-08-02 15:02:17 +0600

    Added example config and .dockerignore file

--- config.example.php
+++ config.example.php
@@ -3,12 +3,12 @@
 $config = [
 	'database' => [
 		'host' => 'localhost',
-		'dbname' => 'explore_php',
+		'dbname' => '',
 		'port' => '5432'
 	],
 	'database_authentication' => [
-		'user' => 'foy4748_php',
-		'password' => 'foy4748_php_db',
+		'user' => '',
+		'password' => '',
 	]
 ];
 
