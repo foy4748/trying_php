edb846ef46645ef645d1fa1ebe5e7c142968e8e6
foy4748 <faisaljfcl@gmail.com>
2024-08-04 12:00:48 +0600

    Solved the issues with CSS file as well as routing
    Thanks to this StackOverflow page
    https://stackoverflow.com/questions/34149470/apache-redirect-all-to-index-php-except-for-existing-files-and-folders

--- views/partials/navbar.partial.php
+++ views/partials/navbar.partial.php
@@ -5,10 +5,13 @@
 	<meta charset="UTF-8">
 	<title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- <link rel="stylesheet" href="/style.css" media="all">  -->
+	<link rel="stylesheet" href="/style.css" media="all">
+
+	<!--
 	<style>
 		<?php include(PROJECT_ROOT.'/style.css'); ?>
-	</style>
+	</style>  
+	-->
 </head>
 
 <body>
